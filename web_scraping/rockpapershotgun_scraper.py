56510f3478626d64c8fff2ca1bfba045ffae9dbd
Emmett <emmettirl@gmail.com>
2024-12-10 00:46:27 +0000

    Added new source https://github.com/Hum-Bao/honkai-star-rail-codes/

--- web_scraping/rockpapershotgun_scraper.py
+++ web_scraping/rockpapershotgun_scraper.py
@@ -1,6 +1,5 @@
 # rockpapershotgun_scraper.py
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
